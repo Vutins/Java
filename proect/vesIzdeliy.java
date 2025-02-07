8eb5763471c273bca80b4bcba45388d8fda1445e
Polina <nikulschina-p@yandex.ru>
2025-02-07 20:45:31 +0400

    Update vesIzdeliy.java

--- proect/vesIzdeliy.java
+++ proect/vesIzdeliy.java
@@ -18,65 +18,67 @@ public class Shopping {
         int vesIzdelGR;
         double vesIzdelKG;
         int corobTkan = 100;
+        
+        while (true) {
+            System.out.println("Здравствуйте Геннадий Юрьевич! Выберите одну из команд (1-4):");
+            System.out.println("1. Посчитать вес изделия.");
+            System.out.println("2. Показать историю изделий.");
+            System.out.println("3. Очистить историю изделий.");
+            System.out.println("4. Завершить работу программы.");
+            int command = scanner.nextInt();
 
-        System.out.println("Здравствуйте Геннадий Юрьевич! Выберите одну из команд (1-4):");
-        System.out.println("1. Посчитать вес изделия.");
-        System.out.println("2. Показать историю изделий.");
-        System.out.println("3. Очистить историю изделий.");
-        System.out.println("4. Завершить работу программы.");
-        int command = scanner.nextInt();
+            switch (command) {
 
-        switch (command) {
+                case 1:
 
-            case 1:
+                    System.out.println("Введите ширину изделия (в метрах): ");
+                    shirina = scanner.nextDouble();
+                    saveShirina[count++] = shirina;
+                    System.out.println("Введите высоту изделия (в метрах): ");
+                    vysota = scanner.nextDouble();
+                    saveVysota[count++] = vysota;
 
-                System.out.println("Введите ширину изделия (в метрах): ");
-                shirina = scanner.nextDouble();
-                saveShirina[count++] = shirina;
-                System.out.println("Введите высоту изделия (в метрах): ");
-                vysota = scanner.nextDouble();
-                saveVysota[count++] = vysota;
+                    if (shirina > 1.6) {
+                        countCronsh = 4;
 
-                if (shirina > 1.6) {
-                    countCronsh = 4;
+                    } else {
+                        countCronsh = 3;
 
-                } else{
-                    countCronsh = 3;
-
-                }
-
-                int vesKarnis = (int) (ves1Karnis * shirina);
-                int vesTkan = (int) ((ves1Tkan * (shirina * vysota))/2);
-                vesIzdelGR = vesKarnis + vesTkan + (countCronsh * ves1Cronsh) + corobTkan;
-                vesIzdelKG = vesIzdelGR / 1000;
-                System.out.println(shirina + " * " + vysota + " вес в граммах: " + vesIzdelGR + ". Вес в килограммах: " + vesIzdelKG);
-                break;
-
-            case 2:
-
-                for(int i = 0; i < 100; i++) {
-
-                    if(saveShirina != null && saveVysota != null && saveVesZakaza != null) {
-
-                        System.out.println((i+1) + ". " + saveShirina[i] + " * " + saveVysota[i] + ". Вес заказа: " + saveVesZakaza[i]);
                     }
-                }
 
-                break;
+                    int vesKarnis = (int) (ves1Karnis * shirina);
+                    int vesTkan = (int) ((ves1Tkan * (shirina * vysota)) / 2);
+                    vesIzdelGR = vesKarnis + vesTkan + (countCronsh * ves1Cronsh) + corobTkan;
+                    vesIzdelKG = vesIzdelGR / 1000;
+                    System.out.println(shirina + " * " + vysota + " вес в граммах: " + vesIzdelGR + ". Вес в килограммах: " + vesIzdelKG);
+                    break;
 
-            case 3:
+                case 2:
 
-                for(int i = 0; i < 100; i++) {
+                    for (int i = 0; i < 100; i++) {
 
-                    saveShirina[i] = Double.parseDouble(null);
-                    saveVysota[i] = Double.parseDouble(null);
-                    saveVesZakaza[i] = Double.parseDouble(null);
-                }
-                break;
+                        if (saveShirina != null && saveVysota != null && saveVesZakaza != null) {
 
-            case 4:
+                            System.out.println((i + 1) + ". " + saveShirina[i] + " * " + saveVysota[i] + ". Вес заказа: " + saveVesZakaza[i]);
+                        }
+                    }
 
-                return;
+                    break;
+
+                case 3:
+
+                    for (int i = 0; i < 100; i++) {
+
+                        saveShirina[i] = Double.parseDouble(null);
+                        saveVysota[i] = Double.parseDouble(null);
+                        saveVesZakaza[i] = Double.parseDouble(null);
+                    }
+                    break;
+
+                case 4:
+
+                    return;
+            }
         }
     }
 }
